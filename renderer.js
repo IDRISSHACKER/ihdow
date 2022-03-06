66c8a5f5810085d646d8b33d9991d5c0077053a3
IDRISSHACKER <idrisscoder@gmail.com>
2022-03-06 11:45:47 +0100

    Ajout des interactions sur la section action

--- renderer.js
+++ renderer.js
@@ -1,81 +1,72 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
-
 const games = [...document.querySelectorAll(".game")]
+const menus = [...document.querySelectorAll(".menu")]
 let activedGame = null
 let oldGame = null
 let newGames = null
 let newGameFilter = []
 
-games.forEach((game) => {
-    game.addEventListener("click", () => {
-        console.log("==================")
-        oldGame = document.querySelector(".gameActive")
-        //gg
-        activedGame = game
-        RemoveActive()
-        //gg
-        newGames = [...document.querySelectorAll(".game")]
-        //gg
-        let count = 1
-        newGames.forEach((jeu) => {
-            if (count == 1) {
+const ManageGameSection = () => {
+    games.forEach((game) => {
+        game.addEventListener("click", () => {
+            oldGame = document.querySelector(".gameActive")
+            //gg
+            activedGame = game
+            RemoveActiveGame()
+            //gg
+            newGames = [...document.querySelectorAll(".game")]
+            //gg
+            let count = 1
+            newGames.forEach((jeu) => {
+                if (count == 1) {
 
-            } else if (jeu !== activedGame) {
-                newGameFilter.push(jeu)
-            }
-            if (count == newGames.length) {
-                newGameFilter = [...newGameFilter, newGames[0]]
-            }
-            count++
+                } else if (jeu !== activedGame) {
+                    newGameFilter.push(jeu)
+                }
+                if (count == newGames.length) {
+                    newGameFilter = [...newGameFilter, newGames[0]]
+                }
+                count++
+            })
+            count = 0
+            const gamesCont = document.querySelector(".games")
+            gamesCont.innerHTML = ""
+            let counter = 1
+            newGameFilter.forEach((jeu) => {
+                gamesCont.appendChild(jeu)
+                if (counter == 2) {
+                    activedGame.classList.add("gameActive")
+                    gamesCont.appendChild(activedGame)
+                }
+                counter++
+            })
+            counter = 0
+            newGameFilter = []
+            activedGame = null
+            oldGame = null
+            newGames = null
+
         })
-        count = 0
-        const gamesCont = document.querySelector(".games")
-        gamesCont.innerHTML = ""
-        let counter = 1
-        newGameFilter.forEach((jeu) => {
-            gamesCont.appendChild(jeu)
-            if (counter == 2) {
-                activedGame.classList.add("gameActive")
-                gamesCont.appendChild(activedGame)
-            }
-            console.log(jeu)
-            counter++
-        })
-        counter = 0
-        newGameFilter = []
-        activedGame = null
-        oldGame = null
-        newGames = null
-
-
-        //RemoveActive()
-        //PermuteGame(activedGame)
-
-        //SetActive(oldGame)
-
     })
-})
-
-const SetActive = (game) => {
-    game.classList.add("gameActive")
 }
 
-const RemoveActive = () => {
+const ManageMenuSection = () => {
+    menus.forEach((menu) => {
+        menu.addEventListener("click", () => {
+            RemoveActiveMenu()
+            menu.classList.add("menuActive")
+        })
+    })
+}
+
+const RemoveActiveGame = () => {
     const activeGame = document.querySelector(".gameActive")
     activeGame.classList.remove("gameActive")
 }
 
-const PermuteGame = (game) => {
-    let activeGame = oldGame
-    const selectedGame = game
-    const htmlActive = activeGame.innerHTML
-
-    activeGame.innerHTML = selectedGame.innerHTML
-    selectedGame.innerHTML = htmlActive
-    //activeGame.innerHTML = selectedGame.innerHTML
+const RemoveActiveMenu = () => {
+    const activeMenu = document.querySelector(".menuActive")
+    activeMenu.classList.remove("menuActive")
 }
+
+ManageGameSection()
+ManageMenuSection()
